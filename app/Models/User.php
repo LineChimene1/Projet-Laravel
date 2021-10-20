3444b248793b02d9008b87d7121a9bc29d60bb63
chimene <djamenchimene@gmail.com>
2021-10-20 11:26:27 +0200

    Revert " creation du modele utilisateur"
    This reverts commit 4f11f2177c38c6adcf77cc538a50e346991e0204.

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-    //:Modification
+
     /**
      * The attributes that are mass assignable.
      *
@@ -31,7 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ]
+    ];
 
     /**
      * The attributes that should be cast.
